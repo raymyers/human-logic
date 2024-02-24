c4b0946d56d57ae0fe72f518615284b0ae84873f
Ray Myers <rmyers@indeed.com>
2024-02-24 17:46:48 -0600

    Use OpenAI to convert to logic syntax

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # FOL Assistant
 
 FOL (First-Order Logic) Assistant is an simple interactive prover for developers learning Formal Methods.
+
+Runs on the terminal.
+
+## Example 1
+1 Everything that begins to exist has a cause
+2 The universe began to exist
+C The universe has a cause
+
+## Example 2
+1 All men are mortal
+2 Socrates was a man
+C Socrates was mortal
+
+## Requirements
+
+* Python 3
+* Z3 for prover support
+
+## Running
+
+```
+pip install -r requrements.txt
+
+python main.py
+``
+
+## Checks
+
+```
+ruff check --fix .
+```
--- main.py
+++ main.py
@@ -1,40 +1,176 @@
 import cmd
 import re
 
+import instructor
+
+import openai
+from openai import OpenAI, APITimeoutError
+from pydantic import BaseModel, ValidationError
+from retry import retry
+
+
+
+OPENAI_CLIENT = None
+MODEL = "gpt-4-0125-preview" # 4 Turbo
+DEBUG = False
+
+def init_openai():
+    if DEBUG:
+        openai.log = "debug"
+    global OPENAI_CLIENT
+    OPENAI_CLIENT = instructor.patch(OpenAI(timeout=30.0))
+
+
+class LogicLine(BaseModel):
+    """
+    Represents a line of logic with its corresponding comment and FOL expression.
+    """
+    comment: str
+    fol: str
+
+class Z3Function(BaseModel):
+    name: str
+    param_types: list[str]
+
+class Z3Line(BaseModel):
+    code: str
+    sorts: list[str]
+    functions: list[Z3Function]
+
+
+class LogicLineWrapper(BaseModel):
+    text: str
+    logic_line: LogicLine | None
+    z3_line: Z3Line | None
+    is_conclusion: bool
+
+class LogicLines(BaseModel):
+    lines: list[LogicLine]
+
+def logic_line_prompt(text: str, context_lines: list[LogicLineWrapper]):
+    context = "\n".join([line.logic_line.fol for line in context_lines if line.logic_line])
+    return f"""
+    ```
+    {text}
+    ```
+    Convert the above into a logic line.
+    A logic line these parts:
+    * comment: text description
+    * fol: First-Order Logic notation, symbols ∀ ¬ ∃ ∧ ∨ → ↔, example:
+        * ∀ x P(x)
+        * ∀ x (Man(x) → Mortal(x))
+        * Mortal(Socrates)
+
+    # Context
+    Be consistent with the names in the previous steps.
+    ```
+    {context}
+    ```
+    """
+
+@retry((ValidationError, APITimeoutError), tries=3, delay=1)
+def generate_logic_lines(
+    text: str,
+    context_lines: list[LogicLine]
+) -> LogicLine | None:
+    if not text.strip():
+        return None
+    prompt = logic_line_prompt(text, context_lines)
+    response = OPENAI_CLIENT.chat.completions.create(
+        model=MODEL,
+        response_model=LogicLine,
+        temperature = 0.2,
+        messages=[
+            {"role": "user", "content": prompt},
+        ]
+    )
+    return response
+
+def format_fol(line: LogicLine):
+    text = line.fol
+    text = text.replace("\f", r'\f')
+    text = text.replace("\r", r'\r')
+    text = text.replace("\t", r'\t')
+    text = text.replace("\n", r'\n')
+    text = text.replace(r'\forall', '∀')
+    text = text.replace(r'\neg', '¬')
+    text = text.replace(r'\exists', '∃')
+    text = text.replace(r'\land', '∧')
+    text = text.replace(r'\lor', '∨')
+    text = text.replace(r'\to', '→')
+    text = text.replace(r'\rightarrow', '→')
+    text = text.replace(r'\leftrightarrow', '↔')
+    text = text.strip()
+    return text
+
+
 class Lang(cmd.Cmd):
     intro = 'Welcome to the FOL Assistant shell. Type help or ? to list commands.\n'
     prompt = 'FOL> '
     file = None
-    lines = []
+    lines: list[LogicLineWrapper] = []
     def do_q(self, arg):
         'Quit: q'
         print('Exiting')
         return True
     
-    def default(self, line):
+    def default(self, line: str):
         if line == 'EOF':
             print()
             return True
-        match = re.match(r'^(\d+)\b(.*)', line)
+        match = re.match(r'^([cC])?(\d+)?\b(.*)', line)
         if match:
-            number, text = match.groups()
-            if int(number) == 0:
-                self.lines.insert(0, text)
+            is_conclusion, number, text = match.groups()
+            is_conclusion = bool(is_conclusion)
+            if is_conclusion:
+                self.lines = [line for line in self.lines if not line.is_conclusion]
+            if is_conclusion or not number:
+                zero_index = len(self.lines)
             else:
-                if int(number) - 1 < len(self.lines):
-                    self.lines[int(number) - 1] = text
+                zero_index = int(number) - 1
+            context_lines = self.lines
+            if zero_index >= 0 and zero_index < len(self.lines):
+                context_lines = self.lines[:zero_index] + self.lines[zero_index+1:]
+            
+            gen_line = LogicLineWrapper(text=text.strip(), z3_line=None, logic_line=None, is_conclusion=is_conclusion)
+            
+            if number == '0':
+                self.lines = [gen_line] + self.lines
+            else:
+               
+                if zero_index < len(self.lines):
+                    self.lines = self.lines[:zero_index] + [gen_line] + self.lines[zero_index+1:]
                 else:
-                    self.lines.append(text)
-                self.lines = [line for line in self.lines if line.strip()]
+                    self.lines.append(gen_line)
+            self.lines = [line for line in self.lines if line and line.text]
         else:
             print(f"ERROR Unknown command `{line}`")
-        self.list_lines()
+        self.do_list()
+        if gen_line.text:
+            print('...')
+            
+            gen_line.logic_line = generate_logic_lines(gen_line.text, context_lines)
+            if DEBUG:
+                print(gen_line.logic_line.json())
+            print()
+            self.do_list()
+        
+        print()
         return False
     
-    def list_lines(self):
-        'List lines with numbers'
+    def do_list(self, _arg=None):
+        'List logic lines as FOL'
         for i, line in enumerate(self.lines):
-            print(f"{i+1:2} {line}")
+            num_text = i+1
+            if line.is_conclusion:
+                num_text = 'C'
+            if line.logic_line:
+                print(f"{num_text} " + format_fol(line.logic_line))
+            else:
+                print(f"{num_text} " + line.text)
+
+
 if __name__ == '__main__':
+    init_openai()
     Lang().cmdloop()
 
